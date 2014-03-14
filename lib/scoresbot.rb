d6ed13db63de9095d956398adaf03aa6460db9ee
Glenn Martin <gmartin@atlassian.com>
2014-03-14 16:59:32 +1100

    fixed some bugs

--- lib/scoresbot.rb
+++ lib/scoresbot.rb
@@ -55,7 +55,7 @@ module Scoresbot
 			# listArray << "'Weekly average:' + ' '    + '#{average}' + ' ' " 
 			# listArray << "'Weekly Standard Div:' + " "   + '#{standardDev}'"
 
-			listArray = "#{teamname}" + "#{bugScore}" 
+			listArray = "hello"
 
 			return listArray
 
