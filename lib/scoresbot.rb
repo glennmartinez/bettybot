5ff556f72c2d9d5ff41c6b76cf937366d1dd98b5
Glenn Martin <gmartin@atlassian.com>
2014-03-17 14:02:28 +1100

    solved conflicts

--- lib/scoresbot.rb
+++ lib/scoresbot.rb
@@ -56,7 +56,7 @@ module Scoresbot
 			# listArray << "'Weekly Standard Div:' + " "   + '#{standardDev}'"
 
 			# listArray = "hello"
-				testmachine = "testing testing"
+				testmachine = "Bugscore:" + "#{bugScore}"
 
 			return testmachine
 
