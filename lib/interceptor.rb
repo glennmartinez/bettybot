d1fc26fdd1f629a68924381da70c30f66fa02ecc
Glenn Martin <gmartin@atlassian.com>
2014-03-17 14:08:04 +1100

    solved conflicts

--- lib/interceptor.rb
+++ lib/interceptor.rb
@@ -9,7 +9,7 @@ module Interceptor
 		 message = request["item"]["message"]["message"]
 
 		 # response = Helpers.botrouter(message)
-		 getscores = Scoresbot.getTeamScore("muppets", 2)
+		 getscores = Scoresbot.getTeamScore("muppets", 10)
 
 		 return getscores			
 
