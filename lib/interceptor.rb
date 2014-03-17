e6989bf19a86ab2e43d80024212803374b8403d9
Glenn Martin <gmartin@atlassian.com>
2014-03-18 09:10:06 +1100

    solved conflicts

--- lib/interceptor.rb
+++ lib/interceptor.rb
@@ -26,7 +26,7 @@ module Interceptor
 		 else 
 
 		 	response = Array.new
-		 	errorCommand = "Unknown command, please type: 'betty scores teamname weeks <img src='http://upload.wikimedia.org/wikipedia/en/5/5e/Betty_Rubble.png'/>"
+		 	errorCommand = "Unknown command, please type: 'betty scores teamname weeks <img src='http://upload.wikimedia.org/wikipedia/en/5/5e/Betty_Rubble.png' width='62' height='120'/>"
 		 	response << "red"
 		 	response << errorCommand
 
