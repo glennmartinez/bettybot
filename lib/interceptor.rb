e53ce8c2c8c2905821ffeda7d80d0f76ebeb6b94
Glenn Martin <gmartin@atlassian.com>
2014-03-17 14:54:32 +1100

    solved conflicts

--- lib/interceptor.rb
+++ lib/interceptor.rb
@@ -8,13 +8,22 @@ module Interceptor
 
 		 message = request["item"]["message"]["message"]
   	 messageArray = message.split
+
+  	 command = messageArray[1].to_s
 		 teamname = messageArray[2].to_s #gets first parameter such as 'teamname'
 		 weeks = messageArray[3] #gets second parameter such as number of weeks
 		 # response = Helpers.botrouter(message)
-		 getscores = Scoresbot.getTeamScore(teamname, weeks.to_i)
+		 if command == "scores"
+			 getscores = Scoresbot.getTeamScore(teamname, weeks.to_i)
 
-		 return getscores			
+			 		 	return getscores			
 
+		 else 
+
+		 	getscores = "This is the best"
+		 	
+		 	return getscores			
+		 end
 
 		
 	end
