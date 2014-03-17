fe2d023c15faf669a7e2f4021e1179ac1cbfe202
Glenn Martin <gmartin@atlassian.com>
2014-03-17 14:27:19 +1100

    solved conflicts

--- lib/interceptor.rb
+++ lib/interceptor.rb
@@ -7,9 +7,10 @@ module Interceptor
 
 
 		 message = request["item"]["message"]["message"]
-
+		 teamname = messageArrau[2].to_s #gets first parameter such as 'teamname'
+		 weeks = messageArray[3] #gets second parameter such as number of weeks
 		 # response = Helpers.botrouter(message)
-		 getscores = Scoresbot.getTeamScore("muppets", 10)
+		 getscores = Scoresbot.getTeamScore(teamname, 10)
 
 		 return getscores			
 
